be5a804fcb2990c2efceaceda8660517df7d9fce
akshaypuri08 <akshaypuri08@gmail.com>
2017-07-12 18:16:05 +0530

    add self info user data

---
+++ constants.py
@@ -0,0 +1,7 @@
+
+# global variables file
+
+# instagram API access token
+APP_ACCESS_TOKEN = 'e39a6ae329084b059e8633cac79d1406'
+# instagram API base url
+BASE_URL = 'https://api.instagram.com/v1/'
